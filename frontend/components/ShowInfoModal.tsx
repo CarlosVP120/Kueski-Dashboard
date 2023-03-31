62a6a1346cb78dfd149e2974a1dce158a7258f69
CarlosVP120 <carlosveryan4@gmail.com>
2023-03-31 09:14:34 -0600

    sticky header in modal

--- frontend/components/ShowInfoModal.tsx
+++ frontend/components/ShowInfoModal.tsx
@@ -9,7 +9,7 @@ const ShowInfoModal = ({
 }) => {
   // Modal screen
   return (
-    <div className="fixed z-10 inset-0 overflow-y-auto animate-appearShort">
+    <div className="fixed z-10 inset-0 overflow-hidden animate-appearShort">
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity" aria-hidden="true">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
@@ -21,15 +21,15 @@ const ShowInfoModal = ({
           &#8203;
         </span>
         <div
-          className="inline-block align-bottom bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle w-4/5"
+          className="inline-block align-bottom bg-white overflow-y-auto  rounded-lg overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle w-4/5 max-h-[90vh]"
           role="dialog"
           aria-modal="true"
           aria-labelledby="modal-headline"
         >
-          <div className="w-full px-4 pt-5 pb-4 ">
+          <div className="w-full px-4 pb-4 ">
             <div className="sm:flex w-full">
               <div className="mt-3 text-center sm:mt-0 w-full">
-                <div className="w-full mb-6 flex items-center justify-between">
+                <div className="w-full bg-white pt-5  mb-6 flex items-center justify-between sticky top-0">
                   <h1
                     className="text-2xl leading-6 font-bold text-gray-900 "
                     id="modal-headline"
