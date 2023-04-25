d5a398e1a5dbac47e73045819d76b48e4a8e0efc
CarlosVP120 <carlosveryan4@gmail.com>
2023-04-25 10:18:17 -0600

    server

--- frontend/components/OposicionBody.tsx
+++ frontend/components/OposicionBody.tsx
@@ -44,7 +44,7 @@ const OposicionBody = ({}: {}) => {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            className={`w-6 h-6 cursor-pointer ${
+            className={`w-6 h-6 ${
               isHovered ? "text-blue-600" : "text-gray-600"
             }`}
             onMouseEnter={() => setIsHovered(true)}
