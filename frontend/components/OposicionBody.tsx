0d73bb417681f886c1b4b8bdad7f521002d1872d
CarlosVP120 <carlosveryan4@gmail.com>
2023-05-02 10:35:28 -0600

    Oposicion Front Finsiehd

--- frontend/components/OposicionBody.tsx
+++ frontend/components/OposicionBody.tsx
@@ -9,6 +9,11 @@ const OposicionBody = ({}: {}) => {
   const [typeOfOpposition, setTypeOfOpposition] = useState("Primarios");
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [currentUser, setCurrentUser] = useState();
+  const [oposicion, setOposicion] = useState("");
+
+  const handleSubmit = () => {
+    setOposicion(oposicion);
+  };
 
   async function Load() {
     // const res = await fetch("https://kueski-users-db.onrender.com/getUsers", {
@@ -314,7 +319,40 @@ const OposicionBody = ({}: {}) => {
                   );
                 })}
           </div>
-          {/* Second column */}
+          {/* Textarea */}
+        </div>
+
+        {/* Divider */}
+        <div className="w-full border-b border-gray-300 mt-6"></div>
+
+        <div>
+          <p className="text-gray-600 text-sm w-9/12 mt-6">
+            Por medio del presente y atendiendo a los derechos ARCO con los que
+            cuenta el usuario &quot;NombreUsuario&quot; está solicitando la
+            cancelación en el uso de su personal datos.
+          </p>
+
+          <p className="text-gray-600 text-sm mt-2 font-bold">
+            Motivo por el que el usuario solicita la cancelación:
+          </p>
+        </div>
+
+        <div className="w-full flex flex-col pt-4 ml-2 mb-6">
+          <textarea
+            className="w-[97%] h-48 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent bg-gray-100 resize-none"
+            placeholder="Escribe aquí..."
+            value={oposicion}
+            onChange={(e) => {
+              setOposicion(e.target.value);
+            }}
+          ></textarea>
+
+          <button
+            className="w-1/4 bg-blue-600 text-white font-bold py-2 px-4 rounded-md mt-4"
+            onClick={() => handleSubmit()}
+          >
+            Enviar
+          </button>
         </div>
       </div>
     </>
