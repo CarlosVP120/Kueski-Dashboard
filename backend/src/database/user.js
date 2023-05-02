37f00119742c3d74f2a221c18c44709910def18f
AlanRuro <A01641426@tec.mx>
2023-05-02 09:57:32 -0500

    Se cambia la estructura de la conexion a la DB

--- backend/src/database/user.js
+++ backend/src/database/user.js
@@ -1,21 +1,4 @@
-const mysql = require("mysql");
-
-const db = mysql.createConnection({
-    host: "kueski.c2k4scjnbyqp.us-east-2.rds.amazonaws.com",
-    user: "admin",
-    password: "admin123",
-    port: "3306",
-    database: "kueski",
-    multipleStatements: true,
-});
-
-db.connect((error) => {
-    if (error) {
-        console.log("Error Connecting to DB");
-    } else {
-        console.log("Successfully Connected to DB");
-    }
-});
+const db = require("./database");
 
 const getColumnNames = (table) => {
     return new Promise((resolve, reject) => {
