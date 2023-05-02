ebc75a43306fad82f190a4f4d36484d5fd7ffed3
diegopartida22 <A01641113@tec.mx>
2023-05-02 11:27:35 -0600

    Add search bar Cancelacion

--- frontend/components/Cancelacion.tsx
+++ frontend/components/Cancelacion.tsx
@@ -1,11 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { auth } from "../firebase/firebaseClient";
 import styles from "../styles/Form.module.css";
+import Searchbar from "./Searchbar";
 
 const Cancelacion = () => {
+  const [isHovered, setIsHovered] = useState(true);
   const [search, setSearch] = useState("");
-  const [isHovered, setIsHovered] = useState(false);
+  const [data, setData] = useState({});
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [currentUser, setCurrentUser] = useState();
   const [cancelacion, setCancelacion] = useState("");
+
+  async function Load() {
+    // const res = await fetch("https://kueski-users-db.onrender.com/getUsers", {
+    const res = await fetch("http://localhost:3001/api/v1/users", {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await res.json();
+    // console.log("loaded data", result);
+    setData(result);
+  }
+
+  useEffect(() => {
+    (async () => await Load())();
+  }, []);
 
   const name = auth.currentUser?.email
     ?.split("@")[0]
@@ -30,61 +52,19 @@ const Cancelacion = () => {
           <h1 className="font-bold pb-2 text-gray-600">
             Selecciona un usuario:
           </h1>
-          <div className="flex flex-row items-center pd-2 gap-2">
-            <div className={`${styles.input_group} !max-w-[400px]`}>
-              <input
-                placeholder="Search"
-                className={styles.input_text}
-                value={search}
-                onChange={(e) => {
-                  setSearch(e.target.value);
-                }}
-              />
-              <span className="icon flex items-center px-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-5 h-5 !cursor-default"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                  />
-                </svg>
-              </span>
-            </div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              className={`w-6 h-6 ${
-                isHovered ? "text-blue-600" : "text-gray-600"
-              }`}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-              />
-            </svg>
-            <div className="flex">
-              <h1
-                className={`${
-                  isHovered ? "block" : "hidden"
-                } bg-gray-200 px-3 py-1 rounded-xl animate-appearShort`}
-              >
-                Enter name, email, CURP or RFC
-              </h1>
-            </div>
-          </div>
+        </div>
+        <div>
+          <Searchbar
+            data={data}
+            search={search}
+            setSearch={setSearch}
+            setShowSuggestions={setShowSuggestions}
+            setCurrentUser={setCurrentUser}
+            isHovered={isHovered}
+            setIsHovered={setIsHovered}
+            showSuggestions={showSuggestions}
+            currentUser={currentUser}
+          />
         </div>
 
         <div>
